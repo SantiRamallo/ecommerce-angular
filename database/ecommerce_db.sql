31332d6711e620afd4f09170acb6ee76a8c6bad5
SantiRamallo <67606452+SantiRamallo@users.noreply.github.com>
2020-12-28 14:03:45 -0300

    Se agrega script de DB

---
+++ database/ecommerce_db.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE ecommerce_development; 
+
+CREATE TABLE ecommerce_development.Roles (
+  id TINYINT auto_increment NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT Role_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE ecommerce_development.Users (
+  id INT auto_increment NOT NULL,
+  username varchar(100) NOT NULL,
+  email varchar(200) NOT NULL,
+  role_id TINYINT NOT NULL,
+  password TEXT NOT NULL,
+  CONSTRAINT User_PK PRIMARY KEY (id),
+  CONSTRAINT User_FK FOREIGN KEY (role_id) REFERENCES ecommerce_development.Roles(id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
